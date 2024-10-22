4cefd33d4b7176c806369b697a813bad6e6fa564
Egor <egorik1212bush@gmail.com>
2024-10-22 15:33:31 +0400

    log_in style

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,6 +3,25 @@
     use Illuminate\Support\Str;
 @endphp
 @include('layouts.header')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Здесь добавьте CSRF-токен -->
+    <title>Отчеты</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+        /* Основные стили */
+        
+
+        
+
+        
+    
+    </style>
+</head>
 <div class="container">
 
 <h1>Все публичные отчеты</h1>
@@ -14,9 +33,9 @@
         <div class="col-md-4">
 
             <div class="card">
-
+            <div class="card-img-wrapp">
             <img src="{{ Storage::url($report->first_page_image) }}" alt="Первая страница отчета" class="img-fluid">
-
+            </div>
 
 
                 <div class="card-body">
